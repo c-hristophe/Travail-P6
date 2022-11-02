3457ea412b66d3dbad26e10e0288ffd90965e701
c-hristophe <c-hr@live.fr>
2022-11-02 14:06:30 +0100

    implémentation like/dislike

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -64,13 +64,13 @@ exports.likeOrDislike = (req, res, next) => {
   if (req.body.like === 1) { 
     Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } })
       .then((sauce) => res.status(200).json({ message: 'Like ajouté !' }))
-      .catch(error => res.status(400).json({ error }));
+      .catch(error => res.status(400).json({ error })); 
   } 
   
   else { 
     // Si l'utilisateur n'aime pas la sauce
     Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++)*-1 }, $push: { usersDisliked: req.body.userId } }) 
       .then((sauce) => res.status(200).json({ message: 'Dislike ajouté !' }))
-      .catch(error => res.status(400).json({ error }));
+      .catch(error => res.status(400).json({ error })); 
   } 
 };
